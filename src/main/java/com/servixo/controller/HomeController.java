7c23fa03a6d84f2fa232ccb8f298d56d32d28f85
PALLAPUPRAVEEN19 <praveenpallapu14@gmail.com>
2026-04-28 03:47:25 +0530

    update

---
+++ src/main/java/com/servixo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.servixo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Servixo Backend API is successfully deployed and running!";
+    }
+}
